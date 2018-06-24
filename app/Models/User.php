bc864db8a3c5fa7acf86cb178a619b949544e9b3
gspsuccess <gspsuccess@163.com>
2018-06-24 00:41:38 +0000

    Author can delete topic

--- app/Models/User.php
+++ app/Models/User.php
@@ -31,4 +31,9 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+    public function isAuthorOf($model)
+    {
+        return $this->id == $model->user_id;
+    }
 }
